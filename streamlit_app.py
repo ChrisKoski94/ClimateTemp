b65882d784a1719754994b1d1ce2396cd493a782
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 20:06:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def main():
     st.title("Climate Insights: A Comprehensive Analysis of Global Temperature Trends and Future Projections")
-    st.write("Climate Insights: A Comprehensive Analysis of Global Temperature Trends and Future Projections")
+    
 
 if __name__ == "__main__":
     main()
