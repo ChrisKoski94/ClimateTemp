b2759fb9c496fbbdd39224340dea235934c83b7f
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 20:16:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def main():
 
     # Displaying the image from the data folder
     image_path = "data/_a12672b9-08f6-45e7-99f5-dad7c20210aa.jpg"
-    st.image(image_path, caption="Caption for the image", use_column_width=True)
+    st.image(image_path, use_column_width=True)
 
 if __name__ == "__main__":
     main()
