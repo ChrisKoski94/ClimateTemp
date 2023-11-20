662e878df03f845ba11ded8772a324a302dce536
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 20:24:14 +0100

    Create project_breakdown.py

---
+++ pages/project_breakdown.py
@@ -0,0 +1,16 @@
+def project_breakdown():
+    st.title("Project Breakdown")
+
+    # Text for the breakdown
+    breakdown_text = """
+    **Chris Kowalski**
+    Report 1 - Exploration, data visualization, and data pre-processing report
+
+    **Betsi Flores**
+    Report 2 – Modeling Report
+
+    **Maroun Hleihel**
+    Report 3 - Challenges and Insights in Climate Modeling
+    """
+
+    st.markdown(breakdown_text)
