aa59a238c1569c25455b197bb0669b904146df17
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 20:18:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,33 @@ def main():
     image_path = "data/_a12672b9-08f6-45e7-99f5-dad7c20210aa.jpg"
     st.image(image_path, use_column_width=True)
 
+    # Add a link to the Project Breakdown page
+    st.sidebar.markdown("[Project Breakdown](#project-breakdown)")
+
+def project_breakdown():
+    st.title("Project Breakdown")
+
+    # Text for the breakdown
+    breakdown_text = """
+    **Chris Kowalski**
+    Report 1 - Exploration, data visualization, and data pre-processing report
+
+    **Betsi Flores**
+    Report 2 – Modeling Report
+
+    **Maroun Hleihel**
+    Report 3 - Challenges and Insights in Climate Modeling
+    """
+
+    st.markdown(breakdown_text)
+
 if __name__ == "__main__":
-    main()
+    # Check for page selection
+    pages = {"Home": main, "Project Breakdown": project_breakdown}
+    selection = st.sidebar.radio("Navigate", list(pages.keys()))
+
+    # Run the selected page
+    pages[selection]()
+
 
 
