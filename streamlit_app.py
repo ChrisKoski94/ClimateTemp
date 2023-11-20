d7d82278c61e6e81b9a699cfc174c9bf81ed96d7
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 20:15:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
-# app.py
+# streamlit_app.py
 import streamlit as st
 
 def main():
     st.title("Climate Insights: A Comprehensive Analysis of Global Temperature Trends and Future Projections")
-    
+
+    # Displaying the image from the data folder
+    image_path = "data/_a12672b9-08f6-45e7-99f5-dad7c20210aa.jpg"
+    st.image(image_path, caption="Caption for the image", use_column_width=True)
 
 if __name__ == "__main__":
     main()
+
 
